272f324d033996353ceee5e625a04369669e5715
Carlos Eduardo - Trabalho <eduardo201184@hotmail.com>
2024-06-12 10:05:36 -0300

    refactor: ajustando o codigo e adicionando mais descrição

--- API Enviar email de contato, comentada para clonagem/app.js
+++ API Enviar email de contato, comentada para clonagem/app.js
@@ -1,40 +1,46 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-dotenv.config(); 
+dotenv.config();
 //carregando as variaveis de ambiente no diretorio, .env.
 //se dentro voce tivesse criado uma pasta para o .env fica dentro, no dotenv.config();
 //voce tem que coloca o path: ''
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.json());
-app.use(cors({ methods: "*", origin: "*", allowedHeaders:"*"})); //configurando o a api para funcionar em url diferente da aplicação
-  app.post('/send', (req, res) =>{
-    
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        auth: { user: process.env.USER, pass: process.env.PASS }
-      })
+app.use(cors({ methods: '*', origin: '*', allowedHeaders: '*' })); //configurando o a api para funcionar em url diferente da aplicação
 
-      transporter.sendMail({
-        from: process.env.USER,
-        to: process.env.USERPESSOAL,
-        replyTo: req.body.email,
-        subject: req.body.assunto,
-        text: `Oi, sou ${req.body.nome}, ${req.body.mensagem}.`,
+const transporter = nodemailer.createTransport({
+  //configuração SMTP do email que voce vai usar
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: { user: process.env.USER, pass: process.env.PASS },
+});
 
-      }).then(info=>{
-          res.send(info);
-      }).catch(error=>{
-        res.send(error)
+app.post('/send', (req, res) => {
+
+  const {nome, email, assunto, mensagem} = req.body
+
+  transporter
+    .sendMail({
+      from: process.env.USER,
+      to: process.env.USERPESSOAL,
+      replyTo: email,
+      subject: assunto,
+      text: `Oi, sou ${nome}, ${mensagem}.`,
       })
-  })
+      .then(info => {
+      res.send(info);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
